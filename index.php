f7e947bc74a1372bae43659375d586a22f9f679d
parassingh22 <51949331+parassingh22@users.noreply.github.com>
2019-07-04 11:11:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,544 @@
+﻿
+<html lang="en">
+<head>
+<title>PARAS SINGH</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Client Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script> 
+	
+	<!-- css files -->
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' /><!-- bootstrap css -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' /><!-- custom css -->
+    <link href="css/font-awesome.min.css" rel="stylesheet"><!-- fontawesome css -->
+	<!-- //css files -->
+	
+	<!-- google fonts -->
+	<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+	<!-- //google fonts -->
+	
+</head>
+<body>
+
+<!-- header -->
+<header>
+	<nav class="navbar">
+		<div class="container">
+			<h1 class="wthree-logo">
+				<a href="index.html" id="logoLink"><i class="fa fa-adjust"></i> <span>WEB</span>studio</a>
+			</h1>
+
+			<!-- menu -->
+			<ul id="menu">
+				<li>
+					<input id="check02" type="checkbox" name="menu" />
+					<label for="check02"><span class="fa fa-bars" aria-hidden="true"></span></label>
+					<ul class="submenu">
+						<li><a href="index.html" class="active">Home</a></li>
+						<li><a href="#about">About Me</a></li>
+						<li><a href="#services">Services</a></li>
+						<li><a href="#experience">Experience</a></li>
+						<li><a href="#portfolio">Portfolio</a></li>
+						<li><a href="#testimonials">Testimonials</a></li>
+						<li><a href="#contact">Contact Me</a></li>
+					</ul>
+				</li>
+			</ul>
+			<!-- //menu -->
+
+		</div>
+	</nav>
+</header>
+<!-- //header -->
+
+<!-- banner -->
+<div id="home" class="banner-w3pvt d-flex">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-7 bnr-txt-w3pvt">
+				<div class="bnr-w3pvt-txt mt-sm-5">
+					<h6>Welcome to my website</h6>
+					<h2>Hello <span>I'm Paras Singh</span></h2>
+					<h4>Web designer - Web developer - Digital Marketer - Feelancer</h4>
+					<h4>The goal isn’t to build a website. The goal is to build your business.</h4>
+					
+					
+					<ul class="social_section_1info mt-4">
+						<li class="mb-2 facebook"><a href="https://www.facebook.com/paras.singh.7106"><span class="fa fa-facebook"></span></a></li>
+						<li class="mb-2 twitter"><a href="https://twitter.com/imparas22"><span class="fa fa-twitter"></span></a></li>
+						<li class="linkedin"><a href="https://www.linkedin.com/in/paras-sippy-739664158/"><span class="fa fa-linkedin"></span></a></li>
+						<li class="instagram"><a href="https://www.instagram.com/obsessed_paras/?hl=en"><span class="fa fa-instagram"></span></a></li>
+					</ul>
+					<a href="#about" class="scroll bnr-btn mr-2">Read more </a>
+					<a href="#contact" class="scroll bnr-btn1">Contact Me </a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- //banner -->
+	
+<!-- about -->
+<section class="about" id="about">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-5 p-0">
+				<img src="images/12.jpeg" alt="" class="img-fluid" />
+			</div>
+			<div class="col-lg-6">
+				<div class="about-right">
+					<h4 class="main">Aba</h4>
+					<p>abcac</p>
+					<div class="row mt-sm-5 mt-4 about-right-inner">
+						<div class="col-sm-4 col-6 myphoto-sign text-center">
+							<img src="images/12.jpg" alt="" class="img-fluid rounded-circle"/>
+							<img src="images/signature.png" alt="" class="img-fluid mt-3"/>
+							<a href="#contact" class="scroll abt-btn">Hire Me </a>
+						</div>
+						<div class="col-sm-7 offset-lg-1">
+							<h4>Personal Info</h4>
+							<p>Hello, My name is <strong>Paras Singh</strong>.I build websites that build your business.</p>
+							<h4 class="mt-4">Skills & Abilities</h4>
+							<div class="progress-one mt-3">
+								<h4 class="progress-tittle">Web Designing</h4>
+								<div class="progress">
+									<div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+							</div>
+							<div class="progress-one my-lg-3">
+								<h4 class="progress-tittle">Digital Marketing</h4>
+								<div class="progress">
+									<div class="progress-bar bg-danger" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+							</div>
+							<div class="progress-one">
+								<h4 class="progress-tittle">Logo Designing</h4>
+								<div class="progress">
+									<div class="progress-bar bg-info" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<h3 class="position">About Me</h3>
+		</div>
+    </div>
+</section>
+<!-- //about -->
+
+<!-- services-->
+<section class="services pt-5" id="services">
+	<div class="container py-lg-5">
+		<p class="paragraph">What I do for you</p>
+		<h3 class="heading mb-sm-5 mb-4">My Services</h3>
+		<div class="row">
+			<div class="col-lg-3 col-sm-6 mb-5">
+			   <div class="panel panel-default">
+				  <div class="panel-thumbnail">
+					<img src="images/s1.png" alt="" class="img-fluid" />
+					<h3>01</h3>
+					<h4 class="mt-3">Web Design</h4>
+					<p class="mt-3">We design your future.
+We design your success.</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6 mb-5">
+			   <div class="panel panel-default">
+				  <div class="panel-thumbnail">
+					<img src="images/s2.png" alt="" class="img-fluid" />
+					<h3>02</h3>
+					<h4 class="mt-3">Web Developer</h4>
+					<p class="mt-3">Design. Innovate. Create.</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6 mb-5">
+			   <div class="panel panel-default">
+				  <div class="panel-thumbnail">
+					<img src="images/s3.png" alt="" class="img-fluid" />
+					<h3>03</h3>
+					<h4 class="mt-3">Logo Desinging</h4>
+					<p class="mt-3">Communicating through design.</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6 mb-5">
+				<div class="panel panel-default">
+				  <div class="panel-thumbnail">
+					<img src="images/s4.png" alt="" class="img-fluid" />
+					<h3>04</h3>
+					<h4 class="mt-3">Digital Marketing</h4>
+					<p class="mt-3">Choose What Matters To Your Business & Your Customers.</p>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //services -->
+
+<!-- process -->
+<section class="experience py-5" id="experience">
+	<div class="container py-3">
+		<p class="paragraph">work experience</p>
+		<h3 class="heading mb-sm-5 mb-4">My Experience</h3>
+		<div class="middile-inner-con">
+			<div class="tab-main mx-auto">
+
+				<input id="tab1" type="radio" name="tabs" checked>
+				<label for="tab1"><img src="images/s1.png" alt="" class="img-fluid" /> Company Name 1</label>
+
+				<input id="tab2" type="radio" name="tabs">
+				<label for="tab2"><img src="images/s2.png" alt="" class="img-fluid" /> Company Name 2</label>
+
+				<input id="tab3" type="radio" name="tabs">
+				<label for="tab3"><img src="images/s4.png" alt="" class="img-fluid" /> Company Name 3</label>
+
+				<section id="content1">
+					<div class="menu-grids">
+						<div class="t-in">
+							<div class="text-info-sec">
+								<div class="service-in text-left">
+									<div class="card" >
+										<div class="card-body p-0">
+										<img src="images/s1.png" alt="" class="img-fluid"/>
+										<img src="images/s2.png" alt="" class="img-fluid"/>
+										<img src="images/s3.png" alt="" class="img-fluid"/>
+											<h3 class="mb-md-4">Company Name</h3>
+											<h5 class="my-3 card-title" data-aos="fade-left">Creative Director in some X Company (2001 to 2002)</h5>
+											<p class="card-text" data-aos="fade-left">abcc</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section id="content2">
+					<div class="menu-grids">
+						<div class="t-in">
+							<div class="text-info-sec">
+								<div class="service-in text-left">
+									<div class="card">
+										<div class="card-body p-0">
+										<img src="images/s2.png" alt="" class="img-fluid" />
+										<img src="images/s3.png" alt="" class="img-fluid" />
+										<img src="images/s4.png" alt="" class="img-fluid" />
+											<h3 class="mb-md-4">Company Name</h3>
+											<h5 class="my-3 card-title">abcacac</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section id="content3">
+					<div class="menu-grids">
+						<div class="t-in">
+							<div class="text-info-sec">
+								<div class="service-in text-left">
+									<div class="card">
+										<div class="card-body p-0">
+											<img src="images/s4.png" alt="" class="img-fluid" />
+											<img src="images/s5.png" alt="" class="img-fluid" />
+											<img src="images/s6.png" alt="" class="img-fluid" />
+											<h3 class="mb-md-4">Company Name</h3>
+											<h5 class="my-3 card-title">Mobile App Developer in some z Company (2006 to 2008)</h5>
+											<p class="card-text">abcac.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //process -->
+
+
+<!-- quote -->
+<section class="quote py-5">
+	<div class="container py-lg-5">
+		<div class="row">
+			<div class="col-lg-8">
+				<h4>" abcccac"</h4>
+				<h5>design - developing - Digital Marketing - freelancer</h5>
+				<img src="images/signature.png" alt="" class="img-fluid"/>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //quote -->
+
+<!-- portfolio -->
+<section class="portfolio py-5" id="portfolio">
+	<div class="container py-lg-5">
+		<p class="paragraph">My recent Work</p>
+		<h3 class="heading mb-sm-5 mb-4">Portfolio</h3>
+		<div class="row">
+			<div class="col-lg-3 col-md-4 col-6 p-0">
+				<div class="box9">
+					<img src="images/portfolio1.jpg" alt="portfolio" class="img-fluid" />
+					<div class="box-content">
+						<h3 class="title">Portfolio 1</h3>
+						<ul class="icon">
+							<li><a href="#"><i class="fa fa-search"></i></a></li>
+							<li><a href="#"><i class="fa fa-link"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-6 p-0">
+				<div class="box9">
+					<img src="images/portfolio2.jpg" alt="portfolio" class="img-fluid" />
+					<div class="box-content">
+						<h3 class="title">Portfolio 2</h3>
+						<ul class="icon">
+							<li><a href="#"><i class="fa fa-search"></i></a></li>
+							<li><a href="#"><i class="fa fa-link"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-6 p-0">
+				<div class="box9">
+					<img src="images/portfolio3.jpg" alt="portfolio" class="img-fluid" />
+					<div class="box-content">
+						<h3 class="title">Portfolio 3</h3>
+						<ul class="icon">
+							<li><a href="#"><i class="fa fa-search"></i></a></li>
+							<li><a href="#"><i class="fa fa-link"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-6 p-0">
+				<div class="box9">
+					<img src="images/portfolio4.jpg" alt="portfolio" class="img-fluid" />
+					<div class="box-content">
+						<h3 class="title">Portfolio 4</h3>
+						<ul class="icon">
+							<li><a href="#"><i class="fa fa-search"></i></a></li>
+							<li><a href="#"><i class="fa fa-link"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-6 p-0">
+				<div class="box9">
+					<img src="images/portfolio5.jpg" alt="portfolio" class="img-fluid" />
+					<div class="box-content">
+						<h3 class="title">Portfolio 5</h3>
+						<ul class="icon">
+							<li><a href="#"><i class="fa fa-search"></i></a></li>
+							<li><a href="#"><i class="fa fa-link"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-6 p-0">
+				<div class="box9">
+					<img src="images/portfolio1.jpg" alt="portfolio" class="img-fluid" />
+					<div class="box-content">
+						<h3 class="title">Portfolio 6</h3>
+						<ul class="icon">
+							<li><a href="#"><i class="fa fa-search"></i></a></li>
+							<li><a href="#"><i class="fa fa-link"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-3 col-md-4 col-6 p-0">
+				<div class="box9">
+					<img src="images/portfolio2.jpg" alt="portfolio" class="img-fluid" />
+					<div class="box-content">
+						<h3 class="title">Portfolio 7</h3>
+						<ul class="icon">
+							<li><a href="#"><i class="fa fa-search"></i></a></li>
+							<li><a href="#"><i class="fa fa-link"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-6 p-0">
+				<div class="box9">
+					<img src="images/portfolio3.jpg" alt="portfolio" class="img-fluid" />
+					<div class="box-content">
+						<h3 class="title">Portfolio 8</h3>
+						<ul class="icon">
+							<li><a href="#"><i class="fa fa-search"></i></a></li>
+							<li><a href="#"><i class="fa fa-link"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //portfolio -->
+
+<!--clients-->
+<section class="client py-5" id="testimonials">
+  <div class="container py-lg-5">
+		<p class="paragraph">what clients say</p>
+		<h3 class="heading mb-sm-5 mb-4">Testimonials</h3>
+		  <div class="row client-slider-slide">
+			<div class="col-lg-6 col-md-6 w3layouts-txt-says mb-md-0 mb-5">
+			  <div class="clients-info-grid">
+				<p>abc </p>
+				<div class="row">
+					<div class="col-lg-2 col-3 pr-0 img-position mt-3">
+					  <img src="images/t1.png" alt="" class="img-fluid">
+					</div >
+					<div class=" col-lg-10 col-9 clients-txt-info">
+					  <h4 class="mt-4">John Herd</h4>
+					  <p>Graphic Designer</p>
+					</div>
+				  </div>
+			  </div>
+			</div>
+			<div class="col-lg-6 col-md-6 w3layouts-txt-says">
+			  <div class="clients-info-grid">
+				<p>abc </p>
+				<div class="row">
+					<div class="col-lg-2 col-3 pr-0 img-position mt-3">
+					  <img src="images/t2.png" alt="" class="img-fluid">
+					</div >
+					<div class="col-lg-10 col-9 clients-txt-info">
+					  <h4 class="mt-4">Mickey</h4>
+					  <p>Graphic Designer</p>
+					</div>
+				  </div>
+			  </div>
+			</div>
+		  </div>
+	</div>
+</section>
+<!--//clients-->
+
+<!-- Contact -->
+<section class="contact py-5" id="contact">
+	<div class="container py-lg-5">
+		<p class="paragraph">Get in touch with me</p>
+		<h3 class="heading mb-sm-5 mb-4">Contact</h3>
+		<div class="contact-form">
+			<div class="row">
+				<div class="col-lg-8">
+					<form name="contactform" id="contactform" method="post" action="#" onSubmit="return(validate());" novalidate="novalidate">
+						<div class="row">
+							<div class="form-group col-md-6 ">
+							  <label>Name</label>
+							  <input type="text" class="form-control" id="name" placeholder="Enter Name" name="name">
+							</div>
+							<div class="form-group col-md-6">
+							  <label>Email</label>
+							  <input type="email" class="form-control" id="email" placeholder="Enter Email" name="email">
+							</div>
+							<div class="form-group col-md-6">
+							  <label>Phone No.</label>
+							  <input type="text" class="form-control" id="phone" placeholder="Enter Phone no." name="phone">
+							</div>
+							<div class="form-group col-md-6">
+							  <label>Subject</label>
+							  <input type="text" class="form-control" id="subject" placeholder="Enter Subject" name="subject">
+							</div>
+							<div class="form-group col-12">
+							  <label>Message</label>
+							  <textarea name="issues" class="form-control" id="iq" placeholder="Enter Your Message Here"></textarea>
+							</div>				
+							<button type="submit" class="btn btn-default">Submit </button>
+						</div>
+					</form>
+				</div>
+				<section class="w3layouts-map col-lg-4 mt-lg-0 mt-5">
+					<iframe src="https://maps.google.com/maps?q=kashipur&t=&z=13&ie=UTF8&iwloc=&output=embed" allowfullscreen=""></iframe>
+				</section>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //Contact -->		
+		
+<!-- footer -->
+<footer class="py-lg-5 py-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-2">
+				<h3 class="wthree-logo mt-3">
+					<a href="index.html" id="logoLink1"><span class="fa fa-adjust"></span> <span>C</span>lient</a>
+				</h3>
+			</div>
+			<div class="col-lg-7 my-lg-0 my-4">
+				<p><span class="fa mr-2 fa-map-marker"></span></p>
+				<p class="mt-2"><span class="fa mr-2 fa-envelope"></span><a href="https://mail.google.com/mail/u/0/#inbox">parass161@gmail.com</a>, <span class="fa ml-3 mr-2 fa-phone"></span>+91-8057082111</p>
+			</div>
+			<div class="col-lg-3 copy-right p-0 text-lg-right">
+			
+				<p>Follow me on social media</p>
+				<ul class="social_section_1info">
+					<li class="mb-2 facebook"><a href="https://www.facebook.com/paras.singh.7106""><span class="fa fa-facebook"></span></a></li>
+					<li class="mb-2 twitter"><a href="https://twitter.com/imparas2"><span class="fa fa-twitter"></span></a></li>
+					<li class="linkedin"><a href="https://www.linkedin.com/in/paras-sippy-739664158/"><span class="fa fa-linkedin"></span></a></li>
+					<li class="istagram"><a href=""https://www.instagram.com/obsessed_paras/?hl=en"><span class="fa fa-istagram"></span></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- //footer -->
+
+<!-- copyright -->
+<div class="copyright py-3 text-center">
+	<div class="container">
+		<p class="my-2">© 2019. All rights reserved | Design by Paras Singh</a>
+		</p>
+	</div>
+</div>
+<!-- copyright -->
+
+<!-- move top -->
+<div class="move-top text-right">
+	<a href="#home" class="move-top"> 
+		<span class="fa fa-angle-up  mb-3" aria-hidden="true"></span>
+	</a>
+</div>
+<!-- move top -->
+
+</body>
+</html>
+
+<?php
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$subject=$_POST['subject'];
+		$message=$_POST['issues'];
+		
+		$c=mysqli_connect("localhost","root","");
+		mysqli_select_db($c,"temp");
+		$s="insert into tem values('$name','$email','$phone','$subject','$message')";
+		mysqli_query($c,$s);
+		
+	?>
+	
